4c942d4be23e80e478201fd36a7ed221d0e1d7d0
luisblluis11 <luisbenitezlluis333@gmail.com>
2020-11-24 22:59:55 -0600

    Agrego pruebas la LM y LI

--- 1_LogicaMinimal/Props_LM.v
+++ 1_LogicaMinimal/Props_LM.v
@@ -2,3 +2,62 @@
   Autor: Luis Felipe Benítez Lluis
   script de proposiciones a demostrar de la 
   sección de lógica minimal  *)
+Proposition LM_a: forall A: Prop, ~~~A <-> ~A.
+Proof.
+intros.
+split.
++ intros.
+  intro.
+  apply H.
+  intro.
+  apply H1.
+  assumption.
++ intros.
+  unfold not.
+  intros.
+  apply H0 in H.
+  assumption.
+Qed.
+
+Proposition LM_b: forall A B: Prop, 
+  ~~(A /\ B) -> ~~A /\ ~~B.
+Proof.
+intros.
+split.
++ intro.
+  apply H.
+  intro.
+  apply H0.
+  destruct H1.
+  assumption.
++ intro.
+  apply H.
+  intro.
+  apply H0.
+  destruct H1.
+  assumption.
+Qed.
+
+Proposition LM_c: forall T, forall A : T->Prop,
+  ~~(forall x : T, A x)-> (forall x : T, ~~ A x).
+Proof.
+intros.
+intro.
+apply H.
+intro.
+apply H0.
+apply H1.
+Qed.
+  
+
+
+
+
+
+
+
+
+
+
+
+  
--- 2_LogicaIntuicionista/Props_LI.v
+++ 2_LogicaIntuicionista/Props_LI.v
@@ -2,3 +2,69 @@
   Autor: Luis Felipe Benítez Lluis
   script de proposiciones a demostrar de la 
   sección de lógica intuicionista  *)
+  
+Lemma Para_a: forall A B : Prop,
+  ~~A -> ~B ->~(A->B).
+Proof.
+  intros.
+  intro.
+  apply H.
+  intro.
+  apply H1 in H2.
+  contradiction.
+Qed.
+
+Lemma Para_b_1: forall A B : Prop,
+  ~(A->B)->~~A.
+Proof.
+  intros.
+  intro.
+  apply H.
+  intros.
+  contradiction.
+Qed.
+
+Lemma Para_b_2: forall A B : Prop,
+  ~(A->B)->~B.
+Proof.
+  intros.
+  intro.
+  apply H.
+  intro.
+  assumption.
+Qed.
+
+Proposition LI_a: forall A B: Prop,
+  ~~(A->B)-> ~~A->~~B.
+Proof.
+  intros.
+  intro.
+  apply H.
+  intro.
+  apply H0.
+  intro.
+  apply H2 in H3.
+  contradiction.
+Qed.
+
+
+Proposition LI_b: forall A B: Prop,
+  (~~A->~~B)->~~(A->B). 
+Proof.
+  intros.
+  intro.
+  apply Para_b_2 in H0 as H1.
+  apply Para_b_1 in H0 as H2.
+  apply H in H2.
+  contradiction.
+Qed.
+
+Proposition LI: forall A B: Prop,
+  (~~A->~~B)<->~~(A->B).
+Proof. 
+  split.
+  apply LI_b.
+  apply LI_a.
+Qed.
+
+ 
